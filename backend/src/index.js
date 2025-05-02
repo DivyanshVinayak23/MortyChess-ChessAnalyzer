4ee421ce17ee5b924bd31dab22a95fe4040d43d1
divyanshv <divyansh.vinayak@gmail.com>
2025-05-02 11:50:35 +0530

    Deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ console.log('Environment variables loaded:', {
 });
 
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 4000; 
 
 const allowedOrigins = [
     'https://mortychess.onrender.com', 
